76c55f38ead0d9830a977f5b3888c5d16ddcd23b
osvathpal <GitHub182>
2019-08-28 01:22:12 +0300

    Wifi already added check to ConnectQR
    PassDPass_V15
    My Network activity layout added
    
    PassDPass_V14
    Share Wifi via picker added

--- app/src/main/java/com/example/passdpass/ConnectShareQR.java
+++ app/src/main/java/com/example/passdpass/ConnectShareQR.java
@@ -8,29 +8,38 @@ import android.graphics.Bitmap;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConnectShareQR extends AppCompatActivity {
 
-
+    private static final String TAG = "PassDPass";
     String ssid;
     String password;
     int type;
@@ -38,6 +47,7 @@ public class ConnectShareQR extends AppCompatActivity {
     String toShare;
     ImageView qrImage;
     String wifiID;
+    int isItSaved=0;
 
     WifiConfiguration conf;
     WifiManager wifiManager;
@@ -46,15 +56,29 @@ public class ConnectShareQR extends AppCompatActivity {
     Button btnAddAndConnect;
     Button btnShare;
     List<WifiConfiguration> listOfSavedWifis;
+    List<WifiConfig> wifiList;
+    int checkSaved = 0;
 
+    TextView email_display;
     FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
     private FirebaseFirestore  db;
+
+    String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect_share_qr);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        String curentemail = user.getEmail();
+
+
+        email_display = findViewById(R.id.emailID_UA);
+        email_display.setText(curentemail);
+
 
         db = FirebaseFirestore.getInstance();
 
@@ -66,16 +90,19 @@ public class ConnectShareQR extends AppCompatActivity {
 
         conf = new WifiConfiguration();
         wifiManager = (WifiManager) this.getApplicationContext().getSystemService(WIFI_SERVICE);
+        wifiManager.setWifiEnabled(true);
+
+
 
         if (getIntent().getStringExtra("wifiSSID") != null) {
             ssid = (getIntent().getStringExtra("wifiSSID"));
-            password =(getIntent().getStringExtra("wifiPassword"));
+            password = (getIntent().getStringExtra("wifiPassword"));
             type = getIntent().getIntExtra("type", 0);
             qrSSID.setText(ssid);
             qrPassword.setText(password);
 
             /* - Barcode generation - */
-            toBarcode = "WIFI:T:WPA;S:"+ssid+";P:"+password+";;";
+            toBarcode = "WIFI:T:WPA;S:" + ssid + ";P:" + password + ";;";
             MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
             try {
                 BitMatrix bitMatrix = multiFormatWriter.encode(toBarcode, BarcodeFormat.QR_CODE, 500, 500);
@@ -89,14 +116,14 @@ public class ConnectShareQR extends AppCompatActivity {
             }
 
 
+        }
 
-
-         }
+        verifyWifi(ssid);
 
         btnShare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                toShare = "The Wifi: "+ssid+"\n"+" The Password: "+password+"";
+                toShare = "The Wifi: " + ssid + "\n" + " The Password: " + password + "";
                 Intent intent = new Intent(Intent.ACTION_SEND);
                 intent.setType("text/plain");
                 intent.putExtra(Intent.EXTRA_SUBJECT, "Sharing Wifi");
@@ -113,7 +140,7 @@ public class ConnectShareQR extends AppCompatActivity {
             public void onClick(View v) {
 
                 wifiID = ssid;
-                String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                wifiList = new ArrayList<>();
 
                 CollectionReference dbWifis = db.collection("wifis");
                 WifiConfig wifiConfig = new WifiConfig();
@@ -122,26 +149,26 @@ public class ConnectShareQR extends AppCompatActivity {
                 wifiConfig.setUserId(userID);
                 wifiConfig.setType(type);
 
+                if (0 == checkSaved) {
+                    dbWifis.add(wifiConfig)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Toast.makeText(ConnectShareQR.this, "Wifi Config added to Database", Toast.LENGTH_LONG).show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(ConnectShareQR.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                                }
+                            });
+                } else {
+                    Toast.makeText(ConnectShareQR.this, "Wifi already in Database. You can update in the My Saved Networks section.", Toast.LENGTH_LONG).show();
+                }
 
-
-                dbWifis.add(wifiConfig)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Toast.makeText(ConnectShareQR.this, "Wifi Config added to Firestore", Toast.LENGTH_LONG).show();
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(ConnectShareQR.this, e.getMessage(), Toast.LENGTH_LONG).show();
-                            }
-                        });
-
-
-                conf.SSID = String.format("\"%s\"",ssid);
-                conf.preSharedKey = String.format("\"%s\"",password);
-
+                conf.SSID = String.format("\"%s\"", ssid);
+                conf.preSharedKey = String.format("\"%s\"", password);
 
 
                 wifiManager.addNetwork(conf);
@@ -150,8 +177,8 @@ public class ConnectShareQR extends AppCompatActivity {
 
                 List<WifiConfiguration> list = wifiManager.getConfiguredNetworks();
 
-                for( WifiConfiguration i : list ) {
-                    if(i.SSID != null && i.SSID.equals(ssid)) {
+                for (WifiConfiguration i : list) {
+                    if (i.SSID != null && i.SSID.equals(ssid)) {
                         wifiManager.disconnect();
                         wifiManager.enableNetwork(i.networkId, true);
                         wifiManager.reconnect();
@@ -159,13 +186,13 @@ public class ConnectShareQR extends AppCompatActivity {
                     }
                 }
 
-
-
                 listOfSavedWifis = wifiManager.getConfiguredNetworks();
 
                 for (WifiConfiguration tmp : listOfSavedWifis) {
                     System.out.println("Saved Networks: " + tmp.SSID + " Pass: " + tmp.preSharedKey);
                 }
+
+                startActivity(new Intent(ConnectShareQR.this, MySavedNetworks.class));
 
             }
         });
@@ -174,4 +201,43 @@ public class ConnectShareQR extends AppCompatActivity {
 
 
     }
+    int verifyWifi(final String ssid){
+
+        wifiList = new ArrayList<>();
+
+        db.collection("wifis").whereEqualTo("userId", userID).get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData()+ " => " + wifiList.size() );
+                                WifiConfig wf = document.toObject(WifiConfig.class);
+                                wifiList.add(wf);
+
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                        int i=1;
+                        for (WifiConfig wf : wifiList) {
+                            String tempSSID = wf.getSsid();
+                            if (tempSSID.equals(ssid)) {
+                                checkSaved = 1;
+                                Toast.makeText(ConnectShareQR.this, "Wifi already in Database..." + i, Toast.LENGTH_LONG).show();
+                                i++;
+                            }
+                        }
+
+                    }
+                });
+
+
+
+
+        return checkSaved;
+    }
+
+
 }
---
+++ app/src/main/java/com/example/passdpass/TAG.java
@@ -0,0 +1,4 @@
+package com.example.passdpass;
+
+class TAG {
+}
