2e12628ad6bca9e757994da22caedccd914e4f93
osvathpal <GitHub182>
2019-08-30 00:14:15 +0300

    My Network: Added QR code generation, delete button, update
    Added Share and QR code generation to Connection List Activity
    
    Update and share button  added to My Network
    
    Wifi already added check to ConnectQR
    
    PassDPass_V15
    My Network activity layout added
    
    PassDPass_V14
    Share Wifi via picker added

---
+++ app/src/main/java/com/example/passdpass/NetworksOnPhone.java
@@ -0,0 +1,14 @@
+package com.example.passdpass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NetworksOnPhone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_networks_on_phone);
+    }
+}
